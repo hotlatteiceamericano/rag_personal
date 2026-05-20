b6d55812c10324f57dd4219d0eb22e10633e1c21
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-05-20 23:07:25 +0800

    model Notion API to structs

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -1,16 +1,36 @@
 use std::result;
 
 use async_trait::async_trait;
+use serde::Deserialize;
 
 pub mod notion;
 
 // internal type to abstract from different sources
-pub struct Document {
-    pub id: String,
+#[derive(Debug)]
+pub struct SourceDoc {
+    pub page_id: String,
+    pub title: String,
+    pub url: String,
+    pub block: Vec<TextBlock>,
+}
+
+#[derive(Debug)]
+pub struct TextBlock {
+    pub heading_path: Vec<String>,
     pub text: String,
+    pub kind: BlockKind,
+}
+
+#[derive(Debug)]
+pub enum BlockKind {
+    Heading(u8),
+    Paragraph,
+    ListItem,
+    Quote,
+    Code,
 }
 
 #[async_trait]
 pub trait Source {
-    async fn fetch(&self) -> anyhow::Result<Vec<Document>>;
+    async fn fetch(&self) -> anyhow::Result<Vec<SourceDoc>>;
 }
--- src/source/notion.rs
+++ src/source/notion.rs
@@ -2,10 +2,11 @@ use std::{env, vec};
 
 use anyhow::Context;
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tracing::info;
 
-use crate::source::{Document, Source};
+use crate::source::{Source, SourceDoc};
 
 pub struct NotionSource {
     client: reqwest::Client,
@@ -25,16 +26,16 @@ impl NotionSource {
 
 #[async_trait]
 impl Source for NotionSource {
-    async fn fetch(&self) -> anyhow::Result<Vec<Document>> {
-        let token = env::var("NOTION_TOKEN").context("NOTION_TOKEN not set")?;
+    async fn fetch(&self) -> anyhow::Result<Vec<SourceDoc>> {
         let url = format!(
             "https://api.notion.com/v1/blocks/{}/children?page_size=100",
-            "4f596d0962ca45b7bf5b3937364a8374"
+            "4f596d0962ca45b7bf5b3937364a8374" // todo: replace and iterate all page ids
         );
 
-        let resp: Value = reqwest::Client::new()
+        let resp: BlockListResponse = self
+            .client
             .get(&url)
-            .bearer_auth(&token)
+            .bearer_auth(&self.token)
             .header("Notion-Version", "2022-06-28")
             .send()
             .await?
@@ -42,8 +43,60 @@ impl Source for NotionSource {
             .json()
             .await?;
 
-        info!("{}", serde_json::to_string_pretty(&resp)?);
+        info!("{:?}", resp);
 
         anyhow::Ok(vec![])
     }
+}
+
+#[derive(Deserialize, Debug)]
+struct BlockListResponse {
+    has_more: bool,
+    results: Vec<Block>,
+    next_cursor: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Block {
+    id: String,
+    has_children: bool,
+    #[serde(flatten)]
+    body: BlockBody,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+enum BlockBody {
+    Known(KnownBlock),
+    Unknown(serde_json::Value),
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum KnownBlock {
+    Paragraph { paragraph: RichTextHolder },
+    Heading1 { heading_1: RichTextHolder },
+    Heading2 { heading_2: RichTextHolder },
+    Heading3 { heading_3: RichTextHolder },
+    BulletedListItem { bulleted_list_item: RichTextHolder },
+    NumberedListItem { numbered_list_item: RichTextHolder },
+    ToDo { to_do: RichTextHolder },
+    Toggle { toggle: RichTextHolder },
+    Quote { quote: RichTextHolder },
+    Callout { callout: RichTextHolder },
+    Code { code: CodeBody },
+}
+
+#[derive(Deserialize, Debug)]
+struct RichTextHolder {
+    rich_text: Vec<RichText>,
+}
+#[derive(Deserialize, Debug)]
+struct RichText {
+    plain_text: String,
+}
+#[derive(Deserialize, Debug)]
+struct CodeBody {
+    rich_text: Vec<RichText>,
+    language: String,
 }
